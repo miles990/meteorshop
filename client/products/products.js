c68f8533f8d9f1643014902059370ebafe110b4c
AlexLee <alexlee7171@gmail.com>
2014-08-08 01:55:57 +0800

    Add the selected category for template

--- client/products/products.js
+++ client/products/products.js
@@ -1,3 +1,7 @@
 Template.products.catnotselected = function(){			      
 	return Session.equals('category', null);
 }
+
+Template.products.category = function(){
+	return Session.get('category');
+}
