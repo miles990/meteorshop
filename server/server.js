ba245dbf4ceebdaa7a5608c7ade4ea5d6e533a1a
AlexLee <alexlee7171@gmail.com>
2014-08-07 16:38:56 +0800

    Add some product data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 Meteor.startup(function(){
 	if(Products.find().count() === 0){
 		Products.insert({thumb:'applepie.jpeg', name:'Apple Pie', desc:'Decadent Apple Pie', price:2.50, catName:'Fruity'});
+		Products.insert({thumb:'blackberrysmoothie.jpeg', name:'Blackberry Smoothie', desc:'Lecious Blackberry Smoothie', price:2.50, catName:'Fruity'});
+		Products.insert({thumb:'mod1.jpg', name:'Ego Twist', desc:'Ego Twist Mod', price:22.50, catName:'Mods'});
+
 	}
 	if(Categories.find().count() === 0){
 		var hwid = Categories.insert({name:'HARDWARE'});
