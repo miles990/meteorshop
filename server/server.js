4af12a7ab38887f832327113e2a0c124e8a9d012
AlexLee <alexlee7171@gmail.com>
2014-08-08 01:28:51 +0800

    Add subcategories data

--- server/server.js
+++ server/server.js
@@ -5,5 +5,17 @@ Meteor.startup(function(){
 	if(Categories.find().count() === 0){
 		var hwid = Categories.insert({name:'HARDWARE'});
 		var juid = Categories.insert({name:'JUICE'});
+		SubCategories.insert({name:'Mods',cat:hwid});
+		SubCategories.insert({name:'Fruity',cat:juid});
+		SubCategories.insert({name:'Sweet',cat:juid});
+	}
+});
+
+Meteor.methods({
+	removeAll:function(){
+		Products.remove({});
+		Categories.remove({});
+		SubCategories.remove({});
+		CartItems.remove({});
 	}
 });
